103da18a3eb2c301e9eb0ee84e20a57a51fa52c9
Elixir Practice App <practice@guest.guest>
2019-09-20 22:40:19 +0000

    corrected palindrome to ignore spaces

--- lib/practice/palindrome.ex
+++ lib/practice/palindrome.ex
@@ -1,9 +1,15 @@
 defmodule Practice.Palindrome do
 
   def checkPalindrome(pstring) do
-    num = pstring |> String.length() |> div(2)
-    var1 = pstring |> String.slice(0..num-1) |> String.upcase()
-    var2 = pstring |> String.slice(-num..-1) |> String.upcase() |> String.reverse() 
+    # remove white spaces and put string in all caps
+    trimmedString = pstring |> String.replace(" ", "") |> String.upcase()
+    
+    # get the length/2 of the string to the nearest whole number
+    num = trimmedString |> String.length() |> div(2)
+
+    # compare the first half and the reverse of the second half
+    var1 = trimmedString |> String.slice(0..num-1)
+    var2 = trimmedString |> String.slice(-num..-1) |> String.reverse() 
     var1 === var2
   end
 
