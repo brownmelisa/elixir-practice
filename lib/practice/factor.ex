d3b670bad1b528c4d93d030718cc65b03fe17bca
Elixir Practice App <practice@guest.guest>
2019-09-20 23:40:23 +0000

    working on factor

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,11 +1,27 @@
 defmodule Practice.Factor do
+  def isPrime(num) do
+    cond do
+      num === 1 ->
+        false
+      num === 2 ->
+        true
+      true ->
+        Enum.filter(Enum.to_list(2..div(num, 2)), fn x -> rem(num, x) == 0 end) 
+        |> length == 0 
+    end
+  end
+
   def factorHelper(x, divisor, factorsList) do  
     cond do
       x === divisor -> 
-        factorsList = factorsList ++ [divisor]
+        if isPrime(divisor) do
+          factorsList = factorsList ++ [divisor]
+        end
         factorsList
       rem(x, divisor) === 0 ->
-        factorsList = factorsList ++ [divisor]
+        if isPrime(divisor) do
+          factorsList = factorsList ++ [divisor]
+        end
         factorHelper(div(x, divisor), divisor, factorsList)
       true -> 
         factorHelper(x, divisor + 1, factorsList)
