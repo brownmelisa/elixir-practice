e56d8808d3e8b8a8b8fb5ee19e7a6beec32b9bfe
Elixir Practice App <practice@guest.guest>
2019-09-21 00:57:36 +0000

    working on factor

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,37 +1,32 @@
 defmodule Practice.Factor do
-  def isPrime(num) do
-    cond do
-      num === 1 ->
-        false
-      num === 2 ->
-        true
-      true ->
-        Enum.filter(Enum.to_list(2..div(num, 2)), fn x -> rem(num, x) == 0 end) 
-        |> length == 0 
-    end
+  def parse_int(text) do
+    if !is_integer(text) do
+      {num, _} = Integer.parse(text)
+      num
+    else 
+      text
+    end  
   end
-
-  def factorHelper(x, divisor, factorsList) do  
+    
+  def factorHelper(y, divisor, factorsList) do  
     cond do
-      x === divisor -> 
-        if isPrime(divisor) do
-          factorsList = factorsList ++ [divisor]
-        end
+      y === divisor -> 
+        factorsList = factorsList ++ [divisor]
         factorsList
-      rem(x, divisor) === 0 ->
-        if isPrime(divisor) do
-          factorsList = factorsList ++ [divisor]
-        end
-        factorHelper(div(x, divisor), divisor, factorsList)
+      rem(y, divisor) === 0 ->
+        factorsList = factorsList ++ [divisor]
+        factorHelper(div(y, divisor), divisor, factorsList)
       true -> 
-        factorHelper(x, divisor + 1, factorsList)
+        factorHelper(y, divisor + 1, factorsList)
     end
   end
 
   def findFactors(x) do
     divisor = 2
     factorsList = []
-    factorHelper(x, divisor, factorsList) 
+    x_int = parse_int(x)
+    factorHelper(x_int, divisor, factorsList) 
+    #"TEST RESULT"
   end
-  
+
 end
